f5e2027e340a090425cb926ff03751b0b4c414e0
Jarryd Dean <jarryd_deane@trimble.com>
2022-03-04 12:55:25 +0200

    Add comments to files where needed. Update README.md and add GitVersion.yml generated using dotnet-gitversion tool. Add GitHubAction Attribute from Nuke to generate GitHub Action workflow

--- README.md
+++ README.md
@@ -5,15 +5,31 @@
 * Nuke.Build is a build tool that enables the build of .Net applications. Because it's just a console application that lives within the repository, it comes with intellisense and version control.
 * Github Actions is a workflow executor. Workflows are triggered by events which execute workflows which are made up of actions.
 
-### Nuke.Build
-
-* Nuke.Build has an attribute call GithubActions which supports generating the Github Actions workflow yaml file for you based on the parameters you provide and the requirements of Targets.
-
 ### Dependencies
 
 * Nuke.Common
+
+### Nuke.Build
+
+* Nuke.Build has an attribute call GithubActions which supports generating the Github Actions workflow yaml file for you based on the parameters you provide and the requirements of Targets.
+* IsLocalBuild is what determines whether to build in Debug or Release mode. IsLocalBuild is set internally by the IsServerBuild variable which is set by the Host class which automatically picks up the environment it's executing in.
+* Nuke has an extension for running Targets in Visual Studio but currently only exists for 2019. A version for Visual 2022 is yet to be released. 
+
+### GitVersion
+
+* To install GitVersion command line tool run "dotnet tool install --global GitVersion.Tool --version 5.* --ignore-failed-sources".
+* Once installed run dotnet-gitversion in a Git directory to ensure the install was successful. It must be run in a git directory to generate the version for the repository.
+* Run "dotnet-gitversion init" and go through the wizard to setup GitVersion which will generate a GitVersion.yml file once you have finished wizard and saved changes.
 
 ### Workflow Statuses
 
 [![CI](https://github.com/Jarryd460/NukeBuildWithGithubActions/actions/workflows/ci.yml/badge.svg)](https://github.com/Jarryd460/NukeBuildWithGithubActions/actions/workflows/ci.yml)
 [![NukeBuildWithGithubActions](https://github.com/Jarryd460/NukeBuildWithGithubActions/actions/workflows/dotnet.yml/badge.svg)](https://github.com/Jarryd460/NukeBuildWithGithubActions/actions/workflows/dotnet.yml)
+
+* The above workflow badges were copied from the Github Actions pages for each workflow.
+
+### References
+
+* Nuke setup with Github Actions: https://dev.to/damikun/the-cross-platform-build-automation-with-nuke-1kmc
+* Nuke with Github: https://www.ariank.dev/create-a-github-release-with-nuke-build-automation-tool/amp/
+* GitVersion: https://github.com/GitTools/GitVersion
--- build/_build/Build.cs
+++ build/_build/Build.cs
@@ -1,33 +1,43 @@
 using Nuke.Common;
 using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
+[GitHubActions(
+    "NukeGeneratedWorkflow",
+    GitHubActionsImage.WindowsLatest,
+    GitHubActionsImage.UbuntuLatest,
+    InvokedTargets = new[] { "Compile", "Test" },
+    OnPushBranches = new[] { "master" },
+    OnPullRequestBranches = new[] { "master" },
+    AutoGenerate = true)]
 [CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
 {
+
     /// Support plugins are available for:
     ///   - JetBrains ReSharper        https://nuke.build/resharper
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
-
     public static int Main() => Execute<Build>(x => x.Compile);
-
+    //IsLocalBuild is what determines whether to build in Debug or Release mode.IsLocalBuild is set internally by the IsServerBuild variable which is set by the Host class which automatically picks up the environment it's executing in
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-    //[GitVersion] readonly GitVersion GitVersion;
+    [GitVersion] readonly GitVersion GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
@@ -56,9 +66,9 @@ class Build : NukeBuild
             DotNetBuild(s => s
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
-                //.SetAssemblyVersion(GitVersion.AssemblySemVer)
-                //.SetFileVersion(GitVersion.AssemblySemFileVer)
-                //.SetInformationalVersion(GitVersion.InformationalVersion)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
                 .EnableNoRestore());
         });
 
